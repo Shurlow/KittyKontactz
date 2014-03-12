1c6819f35d56cfda6b6bedeca4088c94d565d114
bhurlow <bhurlow@gmail.com>
2014-03-12 14:31:24 -0500

    examples etc

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+
+
 var express = require('express')
-var app = express()
 var fs = require('fs')
 
+
+var app = express()
+
+app.use( express.logger('dev') );
+app.set('view engine', 'hjs');
+app.use(express.bodyParser());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+
+var kittybase = require('./kittybase')
+console.log(kittybase)
+
+
 var html = fs.readFileSync('index.html').toString()
-
-
-app.configure(function(){
-  app.set('view engine', 'hjs');
-  app.use(express.bodyParser());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
 
 app.get('/', function(req, res){
   res.send(html);
@@ -19,24 +25,47 @@ app.get('/', function(req, res){
 
 
 app.post('/file-upload', function(req, res) {
-    // get the temporary location of the file
-    var tmp_path = req.files.thumbnail.path;
-    // put files in kittybase
-    var target_path = './public/images/' + req.files.thumbnail.name;
-    // move the file from the temporary location to the intended location
-    fs.rename(tmp_path, target_path, function(err) {
-        if (err) throw err;
-        // delete the temporary file
-        fs.unlink(tmp_path, function() {
-            if (err) throw err;
-            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
-        });
+
+  console.log(req.headers)
+  console.log(req.body)
+  console.log(req.files)
+
+  // get the temporary location of the file
+  var tmp_path = req.files.thumbnail.path;
+
+  // put files in kittybase
+  var target_path = './public/images/' + req.files.thumbnail.name;
+
+  // move the file from the temporary location to the intended location
+  fs.rename(tmp_path, target_path, function(err) {
+    if (err) throw err;
+
+    // maybe save your cat here!
+
+    // delete the temporary file
+    fs.unlink(tmp_path, function() {
+
+      if (err) throw err;
+
+      // mabye you send back the saved cat? 
+      res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
+
     });
+
+  });
+
 });
 
 app.get('/all', function(req, res) {
-var allCats = "hey";
-res.render('index', allCats);
+  var allCats = "hey";
+  res.render('index', allCats);
+})
+
+app.get('/cat4', function(req, res) {
+  
+  var cat4 = kittybase.findCatById(4)
+  res.redner('cat', cat4)
+
 })
 
 app.get('/template', function(req, res) {
---
+++ kittybase.js
@@ -0,0 +1,63 @@
+
+
+/* EXPOSE API */
+
+exports.save = save
+
+function save(obj) {
+
+  // open file
+  // parse it
+  // add obj to other kitties
+  // stringify it 
+  // finally, save it back
+  // return true or false
+
+}
+
+// fre
+
+function getCatByName(name) {
+
+  // open file
+  // parse it
+  // save file output to a variable
+  // e.g. cats = {...}
+
+  /*
+
+  var cutekats = []
+
+  kitties.forEach(function(kat) {
+
+    console.log(kat)
+    if (kat.cuteness >= 3) cutekats.push(kat)
+
+    if (kat.name.indexOf('searchparam') > -1) do xyz
+
+  })
+
+  return cutekats
+
+  for each cat in kitties, 
+  return cat if cat.name === name
+  */
+
+}
+
+var o = {
+  cat: "kitty katkky"
+}
+
+var str = JSON.stringify(o)
+
+console.log(str)
+
+
+
+
+
+
+
+
+
